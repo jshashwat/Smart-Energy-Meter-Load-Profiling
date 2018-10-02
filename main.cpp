1241ffe74db4218fdc349494af99d3db5d7aa05d
zero <shashwatjain8@gmail.com>
2018-10-02 12:38:56 +0530

    Changes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+// Main File for project entry point
 #include <QApplication>
 #include "mainwindow.h"
-
+#include<iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+    cout<<"Start";
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
